f868f2c154e589e57e627e117507afed8afa814f
Yaslinli <74163520+Yaslinli@users.noreply.github.com>
2020-11-27 03:33:22 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,20 @@
-document.getElementById('hamburger').addEventListener('click',)
-function
+function toggleMenu() {
+  var nav = document.querySelector('nav.menu');
+  if (nav.classList.contains('menu--open')) {
+    nav.classList.remove('menu--open');
+  } else {
+    nav.classList.add('menu--open');
+  }
+}
+
+document.querySelector('button.hamburger').addEventListener('click', toggleMenu);
+
+function hamburgerIcon() {
+  var hamburger = document.querySelector('button.hamburger');
+  if (hamburger.classList.contains('hamburgerOpen')) {
+    hamburger.classList.remove('hamburgerOpen');
+  } else {
+    hamburger.classList.add('hamburgerOpen');
+  }
+}
+document.querySelector('button.hamburger').addEventListener('click', hamburgerIcon);
